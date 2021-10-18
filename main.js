09a460b1efb295422ba6c9d153c7ad24f5d9bd50
Ashwin <ashwinthomas370@gmail.com>
2021-10-18 08:19:51 +0530

    many changes done

--- main.js
+++ main.js
@@ -1,13 +1,70 @@
 // hamburger navbar
 function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+// ***********************************************
+
+
+// button to go up
+
+var mybutton = document.getElementById("myButton");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// *************************
 
 
 
-// 
+
+function fun() {
+  document.getElementById("name").value = "";
+  document.getElementById("mail").value = "";
+  document.getElementById("phone").value = "";
+}
+
+
+// about us tabs
+function openTab(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementsByClassName("tablinks")[0].click();
+
+
+
+// modal popup close&reset
+
+$("#myModal").modal("hide");
+$("#myModal").on("hidden.bs.modal", function () {
+  $("#myModal form")[0].reset();
+});
